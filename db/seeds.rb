13ff87bebc386917ac32d8386f56e4d725e01176
Hugo Fortunier <hfortunier@gmail.com>
2020-08-12 15:38:17 +0200

    plus qu'a mettre en forme

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+20.times {
+  Flat.create!(
+    name: Faker::Commerce.product_name,
+    address: Faker::Address.full_address,
+    description: Faker::Quote.famous_last_words,
+    price_per_night: rand(35..200),
+    number_of_guests: ["2","4","6","8"].sample
+  )
+}
